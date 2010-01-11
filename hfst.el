9a3840c163760330d5434a00063bba2e6ffe4312
Kevin Brubeck Unhammer <p.ixiemotion@gmail.com>
2010-01-11 13:42:46 +0100

    more keywords to highlight

--- hfst.el
+++ hfst.el
@@ -20,7 +20,7 @@
 ;; along with this program; if not, write to the Free Software
 ;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-(defconst hfst-version "2010-01-04" "Version of hfst-mode")
+(defconst hfst-version "2010-01-11" "Version of hfst-mode")
 
 ;;;============================================================================
 ;;;
@@ -31,52 +31,54 @@
 
 (defvar hfst-mode-syntax-table
   (let ((hfst-mode-syntax-table (make-syntax-table)))
-    ; comments are start with !
+    ; comments start with !
     (modify-syntax-entry ?!  "<" hfst-mode-syntax-table)
-    ; until end-of-line
+    ; and last until end-of-line
     (modify-syntax-entry ?\n ">!" hfst-mode-syntax-table)
-    (modify-syntax-entry ?\\ "\\" hfst-mode-syntax-table)
-    (modify-syntax-entry ?\" "." hfst-mode-syntax-table)
+    ;; todo: % is the escape character (I think)
+    (modify-syntax-entry ?% "\\" hfst-mode-syntax-table)
+    ;; dots appear as symbols in flag diacritics:
+    (modify-syntax-entry ?. "_" hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?\" "." hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?\\ "\\" hfst-mode-syntax-table)
 ;;    (modify-syntax-entry ?\" "." hfst-mode-syntax-table)
-    (modify-syntax-entry ?\' "$" hfst-mode-syntax-table)
-;;    (modify-syntax-entry ?< "\"" hfst-mode-syntax-table)
-    (modify-syntax-entry ?< "." hfst-mode-syntax-table)
-    (modify-syntax-entry ?> "." hfst-mode-syntax-table)
-    (modify-syntax-entry ?{ "(}" hfst-mode-syntax-table)
-    (modify-syntax-entry ?} "){" hfst-mode-syntax-table)
-    (modify-syntax-entry ?( "()" hfst-mode-syntax-table)
-    (modify-syntax-entry ?) ")(" hfst-mode-syntax-table)
-    (modify-syntax-entry ?[ "(]" hfst-mode-syntax-table)
-    (modify-syntax-entry ?] ")[" hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?\' "$" hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?< "\"" hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?< "." hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?> "." hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?{ "(}" hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?} "){" hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?( "()" hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?) ")(" hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?[ "(]" hfst-mode-syntax-table)
+;;     (modify-syntax-entry ?] ")[" hfst-mode-syntax-table)
     hfst-mode-syntax-table)
   "Syntax table for hfst-mode")
 
 (defconst hfst-font-lock-keywords
-  '(
-    ;; escaped characters
-    ("\\(\\\\->\\)\\|\\(\\\\.\\)"
-     (1 'font-lock-function-name-face nil t)
-     (2 'font-lock-unfontify-region-function nil t))
-    ;; keywords
-    ("\\(\\<ALPHABET\\>\\|Multichar_Symbols\\)"
+  '(;; keywords
+    ("\\Sw\\(Alphabet\\|Multichar_Symbols\\|Sets\\|Definitions\\|Diacritics\\|Rule-variables\\|where\\|in\\|matched\\)\\Sw"
      (1 'font-lock-keyword-face nil t))
+    ;; todo: lexicon names always start with a capital letter, but can
+    ;; you have eg. Æ? or just A-Z?
     ("\\(LEXICON\\) +\\(\\(\\sw\\|\\s_\\)+\\)"
      (1 'font-lock-keyword-face nil t)
      (2 font-lock-variable-name-face))
-    ;; include operator
-    ("\\(?:\\(#include\\)[ \t]+\\)?\\(\"<?.+?>?\"\\)"
-     (1 'font-lock-keyword-face nil t)
-     (2 'font-lock-constant-face nil t))
-    ;; (symbol) variables
-    ("\\(\\([$#]\\)=?\\(?:\\\\\\$\\|\\\\\\#\\|[^ \n\t]\\)+?\\2\\)"
+    ;; flag diacritics:
+    ("@\\sw\\.\\(\\(\\sw\\|\\s_\\)+\\)@" 
      (1 'font-lock-variable-name-face nil t))
-    ;; operators
-    ("\\([_/\\\\^]->\\|[\\^_]?\\(?:<=>?\\|<?=>\\)\\|[:|&!?*+^_]\\|^_\\|||\\|<<\\|__\\)"
-     (1 'font-lock-function-name-face nil t))
-    ;; multi-character symbols
-    ("\\(<.*?>\\)"
-     (1 'font-lock-type-face nil t))
-    )
+    ;; End symbol:
+    ("\\(#\\)"
+     (1 'font-lock-warning-face nil t))
+    ;; escape symbol:
+    ("\\(%\\)\\([^ \t\n]+\\)"
+     (1 'font-lock-warning-face nil t)
+     (2 'font-lock-unfontify-region-function nil t))
+    ;; operators:
+    ;; todo: is the + just another symbol?
+    ("\\(\\(?:<=>?\\|<?=>\\)\\|[:+^>|-/;_]\\)"
+    ; [_/\\\\^]->\\|[\\^_]?\\|^_\\|||\\|<<\\|__
+     (1 'font-lock-function-name-face nil t)))
   "Expressions to highlight in hfst-mode.")
 
 (defun hfst-font ()
@@ -102,15 +104,15 @@ http://www.ling.helsinki.fi/kieliteknologia/tutkimus/hfst/."
 ;;   (make-local-variable 'indent-line-function)
 ;;   (setq indent-line-function 'hfst-indent-line)
   (make-local-variable 'comment-start)  
-  (setq comment-start "% ")
+  (setq comment-start "! ")
   (make-local-variable 'comment-end)
   (setq comment-end "")
   (make-local-variable 'comment-start-skip)
-  (setq comment-start-skip "% *")
+  (setq comment-start-skip "! *")
   (make-local-variable 'completion-ignore-case)
   (setq completion-ignore-case nil)
   (hfst-font)
-  (run-hooks 'hfst-mode-hook))
+  (run-mode-hooks 'hfst-mode-hook))
 
 ;;; Run hooks -----------------------------------------------------------------
 (run-hooks 'hfst-load-hook)
